bea566c59c19da758a080410a633add849393251
Alberto Gracia <alberto11752@hotmail.com>
2023-01-17 15:21:47 +0100

    init cronometro

---
+++ CronometroDosBotones/Interfaces/ICronometro.cs
@@ -0,0 +1,9 @@
+﻿namespace CronometroDosBotones.Interfaces
+{
+    public interface ICronometro
+    {
+        public void Start();
+        public void Stop();
+        public void Pause();
+    }
+}
